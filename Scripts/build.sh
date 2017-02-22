13a8d81485e2f0de5675599ed9648021b00bbcc4
SebastianJay <jgs3cd@virginia.edu>
2017-02-21 21:22:05 -0500

    some travis changes

--- Scripts/build.sh
+++ Scripts/build.sh
@@ -1,12 +1,9 @@
 #! /bin/sh
 
-# Rename $project to your project name
-#
 # NOTE the command args below make the assumption that your Unity project folder is
 #  a subdirectory of the repo root directory, e.g. for this repo "unity-ci-test" 
 #  the project folder is "UnityProject". If this is not true then adjust the 
 #  -projectPath argument to point to the right location.
-project="UnityProject"
 
 ## Run the editor unit tests
 echo "Running editor unit tests"
@@ -15,41 +12,41 @@ echo "Running editor unit tests"
 	-nographics \
 	-silent-crashes \
 	-logFile $(pwd)/unity.log \
-	-projectPath "$(pwd)/$project" \
+	-projectPath "$(pwd)/$UNITY_PROJECT_NAME" \
 	-runEditorTests \
 	-editorTestsResultFile "$(pwd)/Test.xml" \
 	-quit
 
 rc0=$?
 echo "Unit test logs"
-cat $(pwd)/tests.xml
+cat $(pwd)/Test.xml
 # exit if tests failed
 if [$rc0 -ne 0]; then { echo "Failed unit tests"; exit $rc0; } fi
 
 ## Make the builds
 # Recall from install.sh that a separate module was needed for Windows build support
-echo "Attempting build of $project for Windows"
+echo "Attempting build of $UNITY_PROJECT_NAME for Windows"
 /Applications/Unity/Unity.app/Contents/MacOS/Unity \
 	-batchmode \
 	-nographics \
 	-silent-crashes \
 	-logFile $(pwd)/unity.log \
-	-projectPath "$(pwd)/$project" \
-	-buildWindowsPlayer "$(pwd)/Build/windows/$project.exe" \
+	-projectPath "$(pwd)/$UNITY_PROJECT_NAME" \
+	-buildWindowsPlayer "$(pwd)/Build/windows/$UNITY_PROJECT_NAME.exe" \
 	-quit
 
 rc1=$?
 echo "Build logs (Windows)"
 cat $(pwd)/unity.log
 
-echo "Attempting build of $project for OSX"
+echo "Attempting build of $UNITY_PROJECT_NAME for OSX"
 /Applications/Unity/Unity.app/Contents/MacOS/Unity \
 	-batchmode \
 	-nographics \
 	-silent-crashes \
 	-logFile $(pwd)/unity.log \
-	-projectPath "$(pwd)/$project" \
-	-buildOSXUniversalPlayer "$(pwd)/Build/osx/$project.app" \
+	-projectPath "$(pwd)/$UNITY_PROJECT_NAME" \
+	-buildOSXUniversalPlayer "$(pwd)/Build/osx/$UNITY_PROJECT_NAME.app" \
 	-quit
 
 rc2=$?
---
+++ Scripts/post_build.sh
@@ -0,0 +1,12 @@
+#! /bin/sh
+
+winname="${UNITY_PROJECT_NAME}_win.tar.gz"
+osxname="${UNITY_PROJECT_NAME}_osx.tar.gz"
+
+# tar and zip the build folders
+tar -czvf $winname "$(pwd)/Build/windows"
+tar -czvf $osxname "$(pwd)/Build/osx"
+
+# upload the tarballs to the server for storage
+#scp -i $UPLOAD_KEYFILE $winname $UPLOAD_USER@$UPLOAD_HOST:$UPLOAD_PATH
+#scp -i $UPLOAD_KEYFILE $osxname $UPLOAD_USER@$UPLOAD_HOST:$UPLOAD_PATH
