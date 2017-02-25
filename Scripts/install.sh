f10bd333d47bb05c5de8ad13729a72f591cc4b85
SebastianJay <jgs3cd@virginia.edu>
2017-02-24 23:51:04 -0500

    shorter path in tarballs

--- Scripts/install.sh
+++ Scripts/install.sh
@@ -4,13 +4,15 @@
 #  The below link will need to change depending on the version, this one is for 5.5.1
 #  Refer to https://unity3d.com/get-unity/download/archive and find the link pointed to by Mac "Unity Editor"
 echo 'Downloading Unity 5.5.1 pkg:'
-curl -o Unity.pkg http://netstorage.unity3d.com/unity/88d00a7498cd/MacEditorInstaller/Unity-5.5.1f1.pkg
+curl --retry 5 -o Unity.pkg http://netstorage.unity3d.com/unity/88d00a7498cd/MacEditorInstaller/Unity-5.5.1f1.pkg
+if [ $? -ne 0 ]; then { echo "Download failed"; exit $?; } fi
 
 # In Unity 5 they split up build platform support into modules which are installed separately
 # By default, only Mac OSX support is included in the original editor package; Windows, Linux, iOS, Android, and others are separate
 # In this example we download Windows support. Refer to http://unity.grimdork.net/ to see what form the URLs should take
 echo 'Downloading Unity 5.5.1 Windows Build Support pkg:'
-curl -o Unity_win.pkg http://netstorage.unity3d.com/unity/88d00a7498cd/MacEditorTargetInstaller/UnitySetup-Windows-Support-for-Editor-5.5.1f1.pkg
+curl --retry 5 -o Unity_win.pkg http://netstorage.unity3d.com/unity/88d00a7498cd/MacEditorTargetInstaller/UnitySetup-Windows-Support-for-Editor-5.5.1f1.pkg
+if [ $? -ne 0 ]; then { echo "Download failed"; exit $?; } fi
 
 # Run installer(s)
 echo 'Installing Unity.pkg'
--- Scripts/post_build.sh
+++ Scripts/post_build.sh
@@ -4,8 +4,8 @@ winname="${UNITYCI_PROJECT_NAME}_win.tar.gz"
 osxname="${UNITYCI_PROJECT_NAME}_osx.tar.gz"
 
 # tar and zip the build folders
-tar -czvf $winname "$(pwd)/Build/windows"
-tar -czvf $osxname "$(pwd)/Build/osx"
+tar -C "$(pwd)/Build" -czvf $winname "windows"
+tar -C "$(pwd)/Build" -czvf $osxname "osx"
 
 # upload the tarballs to the server for storage
 scp -i "${UPLOAD_KEYPATH}" \
