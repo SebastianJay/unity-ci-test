e2679ae958e0ada9aab84fb2b8fc5b79d8a837dc
SebastianJay <jgs3cd@virginia.edu>
2017-02-25 16:29:55 -0500

    add chmod post build

--- Scripts/post_build.sh
+++ Scripts/post_build.sh
@@ -3,6 +3,11 @@
 winname="${UNITYCI_PROJECT_NAME}_win.tar.gz"
 osxname="${UNITYCI_PROJECT_NAME}_osx.tar.gz"
 
+# set file permissions so builds can run out of the box
+for infile in `find $(pwd)/Build | grep -E '\.exe$|\.dll$'`; do
+	chmod 755 $infile
+done
+
 # tar and zip the build folders
 tar -C "$(pwd)/Build" -czvf $winname "windows"
 tar -C "$(pwd)/Build" -czvf $osxname "osx"
